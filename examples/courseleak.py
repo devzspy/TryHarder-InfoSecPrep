ef068585765740e9cf8e0e0dbe3c9db70c96c18f
0xEmma <emma@0xem.ma>
2020-04-22 05:03:42 -0300

    New cmd

---
+++ examples/courseleak.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from builtins import bot
+
+@commands.command()
+async def courseleak(ctx):
+    msg = "If you wish to report a leak of a offsec course, please DM either the Discord Staff and we can help you handle it, or the Offsec staff which have the Offensive Security Staff role."
+    channel = ctx.channel
+    await channel.send(msg)
+
+def setup(bot):
+    bot.add_command(courseleak)
+
+def teardown(bot):
+    bot.remove_command(courseleak)
