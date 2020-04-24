eabc22b9f1d9881b9d532102dbb659185397c4da
0xEmma <47154306+0xEmma@users.noreply.github.com>
2020-04-24 00:31:02 -0300

    Update courseleak.py

--- examples/courseleak.py
+++ examples/courseleak.py
@@ -4,7 +4,7 @@ from builtins import bot
 
 @commands.command()
 async def courseleak(ctx):
-    msg = "If you wish to report a leak of a offsec course, please DM either the Discord Staff and we can help you handle it, or the Offsec staff which have the Offensive Security Staff role."
+    msg = "If you wish to report a leak of an offsec course, please DM either the Discord Staff and we can help you handle it, or the Offsec staff which have the Offensive Security Staff role."
     channel = ctx.channel
     await channel.send(msg)
 
